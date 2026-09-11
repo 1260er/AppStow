b17ce6204e3d9e48473acd427009f2db43083d44
1260er <app-login@mail.de>
2026-09-11 14:44:36 +0200

    build: initialize Android Java project

---
+++ app/build.gradle.kts
@@ -0,0 +1,34 @@
+plugins { id("com.android.application") }
+
+android {
+    namespace = "de.pritcloud.shortcutlauncher"
+    compileSdk = 35
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    defaultConfig {
+        applicationId = "de.pritcloud.shortcutlauncher"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 1
+        versionName = "0.1.0"
+    }
+
+    buildTypes {
+        debug {
+            applicationIdSuffix = ".dev"
+            versionNameSuffix = "-dev"
+        }
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+}
+
+dependencies {
+    implementation("androidx.drawerlayout:drawerlayout:1.2.0")
+}
