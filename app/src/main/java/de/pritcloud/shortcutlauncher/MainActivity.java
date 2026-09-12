2a534c4e55ac857c7155a8e306315a92004abea7
1260er <app-login@mail.de>
2026-09-12 10:35:52 +0200

    ui: align insets and spacing with ScaleLauncher

--- app/src/main/java/de/pritcloud/shortcutlauncher/MainActivity.java
+++ app/src/main/java/de/pritcloud/shortcutlauncher/MainActivity.java
@@ -1,7 +1,10 @@
 package de.pritcloud.shortcutlauncher;
 
 import android.app.Activity;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.DisplayCutout;
+import android.view.View;
 import android.view.Gravity;
 import android.widget.TextView;
 
@@ -17,6 +20,44 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        View contentRoot = findViewById(android.R.id.content);
+        contentRoot.setOnApplyWindowInsetsListener((view, insets) -> {
+            int left;
+            int top;
+            int right;
+            int bottom;
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+                android.graphics.Insets safeInsets = insets.getInsets(
+                        android.view.WindowInsets.Type.systemBars()
+                                | android.view.WindowInsets.Type.displayCutout());
+
+                left = safeInsets.left;
+                top = safeInsets.top;
+                right = safeInsets.right;
+                bottom = safeInsets.bottom;
+            } else {
+                left = insets.getSystemWindowInsetLeft();
+                top = insets.getSystemWindowInsetTop();
+                right = insets.getSystemWindowInsetRight();
+                bottom = insets.getSystemWindowInsetBottom();
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+                    DisplayCutout cutout = insets.getDisplayCutout();
+                    if (cutout != null) {
+                        left = Math.max(left, cutout.getSafeInsetLeft());
+                        top = Math.max(top, cutout.getSafeInsetTop());
+                        right = Math.max(right, cutout.getSafeInsetRight());
+                        bottom = Math.max(bottom, cutout.getSafeInsetBottom());
+                    }
+                }
+            }
+
+            view.setPadding(left, top, right, bottom);
+            return insets;
+        });
+
 
         drawerLayout = findViewById(R.id.drawerLayout);
         pageTitle = findViewById(R.id.pageTitle);
