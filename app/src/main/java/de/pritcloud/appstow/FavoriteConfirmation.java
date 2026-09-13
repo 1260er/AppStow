d76fddef78fd3b1a5a5da1e0a50358f163dbd7ba
1260er <app-login@mail.de>
2026-09-13 22:45:35 +0200

    ui: style favorite removal as destructive

--- app/src/main/java/de/pritcloud/appstow/FavoriteConfirmation.java
+++ app/src/main/java/de/pritcloud/appstow/FavoriteConfirmation.java
@@ -3,6 +3,8 @@ package de.pritcloud.appstow;
 import android.app.AlertDialog;
 import android.content.Context;
 
+import androidx.core.content.ContextCompat;
+
 final class FavoriteConfirmation {
 
     private FavoriteConfirmation() {
@@ -13,20 +15,45 @@ final class FavoriteConfirmation {
             CharSequence name,
             Runnable onConfirm) {
 
-        new AlertDialog.Builder(context)
-                .setTitle(
-                        R.string.favorite_remove_title)
-                .setMessage(
-                        context.getString(
-                                R.string.favorite_remove_message,
-                                name))
-                .setPositiveButton(
-                        R.string.favorite_remove_confirm,
-                        (dialog, which) ->
-                                onConfirm.run())
-                .setNegativeButton(
-                        R.string.action_cancel,
-                        null)
-                .show();
+        AlertDialog dialog =
+                new AlertDialog.Builder(context)
+                        .setTitle(
+                                R.string.favorite_remove_title)
+                        .setMessage(
+                                context.getString(
+                                        R.string.favorite_remove_message,
+                                        name))
+                        .setPositiveButton(
+                                R.string.favorite_remove_confirm,
+                                (currentDialog, which) ->
+                                        onConfirm.run())
+                        .setNegativeButton(
+                                R.string.action_cancel,
+                                null)
+                        .create();
+
+        dialog.setOnShowListener(ignored -> {
+            int neutralColor =
+                    ContextCompat.getColor(
+                            context,
+                            R.color.ui_text_primary);
+
+            int dangerColor =
+                    ContextCompat.getColor(
+                            context,
+                            R.color.ui_danger);
+
+            dialog.getButton(
+                            AlertDialog.BUTTON_NEGATIVE)
+                    .setTextColor(
+                            neutralColor);
+
+            dialog.getButton(
+                            AlertDialog.BUTTON_POSITIVE)
+                    .setTextColor(
+                            dangerColor);
+        });
+
+        dialog.show();
     }
 }
